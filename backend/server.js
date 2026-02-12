0fcaaf9f9351d6121ae7e81435fc581d5c3e08be
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-02-12 12:03:04 +0530

    orgin added in cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 
 app.use("/api/media", mediaRoutes);
 
